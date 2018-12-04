ee6d13b11bab60bdf496f497e0c228d9b3c72bd3
Victoria <exuberantram@gmail.com>
2018-12-03 19:05:52 -0500

    removed disconnected .java classes and began restructuring layout .xml files. Added functionality to MainActivity.java

--- app/src/main/java/com/example/victoria/woodcalculator/MainActivity.java
+++ app/src/main/java/com/example/victoria/woodcalculator/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.victoria.woodcalculator;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wc_thickness_length);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+
+        //creates a listener that sends the user to AddWidth on click
+        Button btn = findViewById(R.id.calcButton1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this, wcAddWidth.class));
+            }
+        });
+
+        //creates a listener that directs the user to woodcademy.com on click
+        ImageButton btnweb = findViewById(R.id.woodcademyFull);
+
+        btnweb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://woodcademy.com"));
+                startActivity(browserIntent);
+            }
+        });
+    }
+
+    public void wc_add_width(View v) {
+        Intent intent = new Intent(this, wcAddWidth.class);
+        startActivity(intent);
     }
 }
