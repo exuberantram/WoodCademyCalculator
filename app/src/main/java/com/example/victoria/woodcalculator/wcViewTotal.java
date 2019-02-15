c65a47ba662efbf5651fa8fa7c570e974d58cda9
Victoria <exuberantram@gmail.com>
2019-02-15 10:27:15 -0500

    Adding button listners to all pages with the Woodcademy Logo

--- app/src/main/java/com/example/victoria/woodcalculator/wcViewTotal.java
+++ app/src/main/java/com/example/victoria/woodcalculator/wcViewTotal.java
@@ -1,7 +1,12 @@
 package com.example.victoria.woodcalculator;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class wcViewTotal extends AppCompatActivity {
 
@@ -10,4 +15,19 @@ public class wcViewTotal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wc_view_total);
     }
-}
+
+    public void addListenerOnButton() {
+        //creates a listener that directs the user to woodcademy.com on click
+        ImageButton btnweb = findViewById(R.id.woodcademyFull);
+
+        btnweb.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://woodcademy.com"));
+                startActivity(browserIntent);
+            }
+        });
+    }
